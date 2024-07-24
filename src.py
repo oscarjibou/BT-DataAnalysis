1628e9bea2d530504c648f12034f3caed1e43a9a
oscarjibou <oscarjibou@gmail.com>
2024-07-24 19:26:56 +0200

    merge de changes a main

--- src.py
+++ src.py
@@ -1,17 +1,28 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+file = "Tractores_Matriculados.csv"
 
 # load raw_data
-raw_data = pd.read_csv("Tractores_Matriculados.csv")
-raw_data.head()
+raw_data = pd.read_excel("Datos_Market_copy.xlsx")
+print(raw_data.head())
 
-# Filter data by year
-start_year = "1985"
-end_year = "1996"
+# print(raw_data.dtypes)
 
-data = raw_data[(raw_data["Fecha"] >= start_year) & (raw_data["Fecha"] <= end_year)]
+# selected the first 100 rows
+selected_data = raw_data.head(100)
 
-data.Tractores.plot(
-    x="Fecha", y="Tractores", kind="line", title="Tractores matriculados por año"
+plt.figure(figsize=(12, 6))
+plt.plot(
+    selected_data["value.sales"],
+    selected_data["unit.sales"],
+    marker="o",
+    linestyle="--",
 )
+plt.xlabel("Price")
+plt.ylabel("Units")
+plt.title("Price vs Units")
+plt.grid(True)
 plt.show()
